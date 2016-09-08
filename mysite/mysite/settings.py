036f0b2ce4ef007f7e2d171b24a1fdc55d78cff9
chenbin <chenbkeep@163.com>
2016-09-08 15:03:14 +0800

    Django投票系统搭建完成

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+#mysql数据库，首先要安装python的mysql模块
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'polls',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # ip
+        'PORT': '3306',
     }
 }
 
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,10 +4,13 @@ from . import views
 urlpatterns = [
     # ex : /polls/
     url(r'^$', views.index, name='index'),
-    # ex : /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex : /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex : /polls/5/vote
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'results/$', views.results, name='results'),
+    # # ex : /polls/5/
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # # ex : /polls/5/results/
+    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # # ex : /polls/5/vote
+    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    url(r'vote_form/$', views.vote_form, name='vote_form'),
 ]
